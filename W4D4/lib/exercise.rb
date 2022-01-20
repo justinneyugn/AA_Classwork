1ab984a2cd125c51b4d390b8e900101bd4481b08
Justin Nguyen <judnguyen@ucdavis.edu>
2022-01-20 15:15:40 -0800

    Add two_sum spec and method

--- W4D4/lib/exercise.rb
+++ W4D4/lib/exercise.rb
@@ -8,5 +8,15 @@ class Array
         unique
     end
 
+    def two_sum
+        eq_zero = []
+        (0...self.length).each do |idx1|
+            (0...self.length).each do |idx2|
+                eq_zero << [idx1, idx2] if idx2 > idx1 && self[idx1] + self[idx2] == 0
+            end
+        end
+        eq_zero
+    end
+
 end
 
--- W4D4/spec/exercise_spec.rb
+++ W4D4/spec/exercise_spec.rb
@@ -7,4 +7,16 @@ describe Array do
             expect([1, 2, 1, 3, 3].uniq).to eq([1, 2, 3])
         end
     end
+
+    describe "#two_sum" do 
+        it "finds all pairs of positions where the elements sum to zero" do 
+            expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+        end
+
+        it "smaller first elements come first" do 
+            [[0, 4], [2, 3]].each do |sub|
+                expect(sub[0]).to be <= sub[1]
+            end
+        end
+    end
 end
