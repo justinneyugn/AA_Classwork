3e3a094b4100dc3f69a7a974e25fc7993fe1d2a9
Justin Nguyen <judnguyen@ucdavis.edu>
2022-01-21 14:27:51 -0800

    Add anagram phase 1

--- W4D5/anagrams.rb
+++ W4D5/anagrams.rb
@@ -1,22 +1,12 @@
-
-
 def first_anagram?(string1, string2)
-  anagrams = []
-  # string1.each_char.with_index do |letter, i|
-    # substr = string1[i+1..-1].split
-    # until substr == string1[i+1..-1] 
-    #   new_anagram = letter + substr.join
-    #   anagrams << new_anagram if !anagrams.include?(new_anagram)
-    #   substr.rotate
-    # end
-  # end
-  string1.each_char.with_index do |letter1, i|
-    string1.each_char.with_index do |letter2, j|
-      anagrams << string1[i..j] if string1[i..j].length == string1.length
-    end
-  end
-
-  p anagrams
+  str1_arr = string1.split("")
+  anagrams = str1_arr.permutation.map { |word| word.join("")}
+  anagrams.include?(string2)
 end
 
-first_anagram?("cat","heh")
+
+p first_anagram?("cat","tca")
+p first_anagram?("gizmo", "sally")    #=> false
+p first_anagram?("elvis", "lives")    #=> true
+
+
