4dc31d9e953120a14e651402247ab540c9438ebd
Justin Nguyen <judnguyen@ucdavis.edu>
2022-01-21 11:53:45 -0800

    Add more methods to test Big O

--- W4D5/big_o.rb
+++ W4D5/big_o.rb
@@ -1,3 +1,4 @@
+require "byebug"
 # This is O(n**2)
 def my_min1(array)
   array.each_with_index do |num1, i|
@@ -38,11 +39,36 @@ def largest_contiguous_subsum1(list)
   big_array.sum
 end
 
+def largest_contiguous_subsum2(list)
+  debugger
+  curr_max = max_so_far = list[0]
+  list.each_with_index do |ele, i|
+    if list[0..i].sum > curr_max
+      curr_max =  list[0..i].sum
+    elsif list[i..-1].sum > curr_max
+      curr_max = list[i..-1].sum
+    elsif list[i] > curr_max
+      curr_max = list[i]
+    end
+    max_so_far = curr_max if curr_max > max_so_far
+  end
+  curr_max
+  # arr = []
+  # (0..list.length).each do |i|
+  #   arr = arr + list.combination(i).to_a
+  # end
+  # p arr
+  # large = arr.first.sum
+  # arr.each { |subarray| large = subarray.sum if subarray.sum > large }
+  # large
+end
+
+
 list = [-5, -1, -3]
-p largest_contiguous_subsum1(list)
+p largest_contiguous_subsum2(list) # => -1
 
-list2 = [2, 3, -6, 7, -6, 7]
-p largest_contiguous_subsum1(list2) # => 8 (from [7, -6, 7])
+# list2 = [2, 3, -6, 7, -6, 7]
+# p largest_contiguous_subsum2(list2) # => 8 (from [7, -6, 7])
 
-list3 = [5, 3, -7]
-p largest_contiguous_subsum1(list3) # => 8
+# list3 = [5, 3, -7]
+# p largest_contiguous_subsum2(list3) # => 8
