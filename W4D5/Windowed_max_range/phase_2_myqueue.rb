e8b5a93420ca6720b4c5021b04c258c96955c0f7
Justin Nguyen <judnguyen@ucdavis.edu>
2022-01-21 16:43:16 -0800

    Create MyQueue class

--- W4D5/Windowed_max_range/phase_2_myqueue.rb
+++ W4D5/Windowed_max_range/phase_2_myqueue.rb
@@ -1,3 +1,25 @@
 class MyQueue
+    def initialize
+        @store = []
+    end
 
+    def peek
+        @store[-1]
+    end
+
+    def size
+        @store.length
+    end
+
+    def empty?
+        @store.empty?
+    end
+
+    def enqueue(num)
+        @store.push(num) 
+    end
+
+    def dequeue
+        @store.shift
+    end
 end
