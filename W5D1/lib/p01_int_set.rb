9d7384aadff40d30836f1a018075218f6cde38bb
Kevin Chen <kevinch28@gmail.com>
2022-01-24 17:15:17 -0500

    add IntSet class and MaxInitSet class

--- W5D1/lib/p01_int_set.rb
+++ W5D1/lib/p01_int_set.rb
@@ -1,19 +1,31 @@
 class MaxIntSet
+
+  attr_reader :store
+
   def initialize(max)
+    @max = max
+    @store = Array.new(max)
   end
 
   def insert(num)
+    raise "Out of bounds" if !is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    return true if @store[num] == true
+    false
   end
 
   private
 
   def is_valid?(num)
+    return false if num < 0 || num >= @max
+    true
   end
 
   def validate!(num)
@@ -27,12 +39,19 @@ class IntSet
   end
 
   def insert(num)
+    i = num % num_buckets
+    @store[i] << num
   end
 
   def remove(num)
+    i = num % num_buckets
+    @store[i].delete(num)
   end
 
   def include?(num)
+    i = num % num_buckets
+    return true if @store[i].include?(num)
+    false
   end
 
   private
