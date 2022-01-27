9cd86a5e6246fde5032690cd7c1b4d696d254ff8
SVKratzer <lukekpat@gmail.com>
2022-01-27 16:47:22 -0500

    Adds associations

--- W5D4/app/models/user.rb
+++ W5D4/app/models/user.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+  has_many(:enrollments, class_name: :Enrollment, foreign_key: :student_id, primary_key: :id)
+
+  has_many :enrolled_courses, through: :enrollments, source: :course
+
+  has_one(:course, class_name: :User, foreign_key: :instructor_id, primary_key: :id)
+  
 end
