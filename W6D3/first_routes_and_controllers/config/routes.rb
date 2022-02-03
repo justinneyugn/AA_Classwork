f959eb27a9de306ea5df06ec8765af8a7641f744
Justin Nguyen <judnguyen@ucdavis.edu>
2022-02-03 14:46:25 -0800

    Add Artwork Shares Controller

--- W6D3/first_routes_and_controllers/config/routes.rb
+++ W6D3/first_routes_and_controllers/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, except: [:new]
+  resources :users, only: [:create, :destroy, :index, :show, :update]
   # get 'users/:id', to: 'users#show', as: 'user'
 
   # get '/users', to: 'users#index'
