85660f1944032fc911b2b52838307b040bff86a2
Justin Nguyen <judnguyen@ucdavis.edu>
2022-02-06 22:01:23 -0800

    Create color constant for cat model

--- W6D5/nindynine_kitties/app/models/cat.rb
+++ W6D5/nindynine_kitties/app/models/cat.rb
@@ -1,11 +1,12 @@
 require 'action_view'
 
 class Cat < ApplicationRecord 
-    # i think we need require 'date'
     include ActionView::Helpers::DateHelper
 
+    CAT_COLORS = %w(black white orange brown).freeze
+
     validates :birth_date, presence: true
-    validates :color, presence: true, inclusion: {in: ["red","orange", "yellow", "green", "blue","brunette"]}
+    validates :color, presence: true, inclusion: CAT_COLORS
     validates :name, presence: true 
     validates :sex, presence: true, inclusion: {in: ["M", "F"]}
     validates :description, presence: true
