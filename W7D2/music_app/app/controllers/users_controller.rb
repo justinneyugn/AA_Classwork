c088c154cc2dd1dcceafd62f6d44d5bbdb5c9dea
Justin Nguyen <judnguyen@ucdavis.edu>
2022-02-08 11:21:56 -0800

    Create Users Controller

--- W7D2/music_app/app/controllers/users_controller.rb
+++ W7D2/music_app/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_logged_out, only: [:new, :create]
+  before_action :requre_logged_in, only: [:index, :show]
+  
   def index
     @users = User.all
     render :index
@@ -24,13 +27,33 @@ class UsersController < ApplicationController
 
     if @user.save
       login!(@user)
-      redirect_to 
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors]  = @user.errors.full_messages
+      render :new
     end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect to "/users/#{@user.id}"
+    else
+      flash.now[:errors]  = @user.errors.full_messages
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to users_url
+    else
+      flash.now[:errors]  = @user.errors.full_messages
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
