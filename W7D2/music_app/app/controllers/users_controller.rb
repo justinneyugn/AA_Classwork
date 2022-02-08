bc8443c119b75349e0b7ad72587212f09abdc983
Justin Nguyen <judnguyen@ucdavis.edu>
2022-02-08 11:24:13 -0800

    Refactor Users Controller for just signing up

--- W7D2/music_app/app/controllers/users_controller.rb
+++ W7D2/music_app/app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_logged_out, only: [:new, :create]
-  before_action :requre_logged_in, only: [:index, :show]
-  
-  def index
-    @users = User.all
-    render :index
-  end
+  before_action :requre_logged_in, only: [:show]
 
   def show
     @user = User.find(params[:id])
@@ -17,11 +12,6 @@ class UsersController < ApplicationController
     render :new
   end
 
-  def edit
-    @user = User.find(params[:id])
-    render :edit
-  end
-
   def create
     @user = User.new(user_params)
 
@@ -31,24 +21,6 @@ class UsersController < ApplicationController
     else
       flash.now[:errors]  = @user.errors.full_messages
       render :new
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect to "/users/#{@user.id}"
-    else
-      flash.now[:errors]  = @user.errors.full_messages
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    if @user.destroy
-      redirect_to users_url
-    else
-      flash.now[:errors]  = @user.errors.full_messages
     end
   end
 
