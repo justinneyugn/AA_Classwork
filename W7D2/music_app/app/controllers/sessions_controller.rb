d464539aeb8c403cd105ad5966e6f36ed465fb16
Justin Nguyen <judnguyen@ucdavis.edu>
2022-02-08 11:40:28 -0800

    Add Sessions Controller

--- W7D2/music_app/app/controllers/sessions_controller.rb
+++ W7D2/music_app/app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = ['Invalid Username or Password']
+      render :new
+    end
   end
 
   def destroy
+    logout!
+    redirect_to new_session_url
   end
 
   def new
+    @user = User.new
+    render :new
   end
 end
