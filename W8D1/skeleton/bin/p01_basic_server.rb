48bc184cccfefa1cb726ce0db833e7de891dc7ce
Justin Nguyen <judnguyen@ucdavis.edu>
2022-02-14 15:41:14 -0800

    Complete the Basic Controller and Controller Server

--- W8D1/skeleton/bin/p01_basic_server.rb
+++ W8D1/skeleton/bin/p01_basic_server.rb
@@ -1,1 +1,15 @@
 require 'rack'
+app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new
+    path = req.path
+    res['Content-Type'] = 'text'
+    res.write(path)
+    res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+
+)
--- W8D1/skeleton/lib/controller_base.rb
+++ W8D1/skeleton/lib/controller_base.rb
@@ -8,20 +8,39 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    if @already_built_response.nil?
+      @already_built_response = false
+    else
+      @already_built_response = true
+      raise "You suck"
+    end
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if !already_built_response?
+      @res.status = 302
+      @res["Location"] = url
+    end
+    @res
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    if !already_built_response?
+      @res.write(content)
+      @res['Content-Type'] = content_type
+    end
+    @res
   end
 
   # use ERB and binding to evaluate templates
