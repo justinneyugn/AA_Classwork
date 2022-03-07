0bf002ebc2f1a99ed290e7dde00c1f0518d8a106
Justin Nguyen <judnguyen@ucdavis.edu>
2022-03-07 13:28:17 -0800

    Complete all setup for React

--- W11D1/webpack.config.js
+++ W11D1/webpack.config.js
@@ -2,7 +2,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: './frontend/react_minesweeper.jsx',
+    entry: './react_minesweeper.jsx',
     output: {
     filename: './bundle.js',
     },
