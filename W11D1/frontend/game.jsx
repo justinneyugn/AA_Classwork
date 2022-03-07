3e7a1e3fdbe87af14fb60b331a8e4c4f9a2a969e
Justin Nguyen <judnguyen@ucdavis.edu>
2022-03-07 13:59:00 -0800

    Create Game component

--- W11D1/frontend/game.jsx
+++ W11D1/frontend/game.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import * as Minesweeper from "./minesweeper.js";
 
 class Game extends React.Component{
     constructor(){
         super();
-        
+        this.state = {board: new Minesweeper.Board(9, 10)};
+    }
+
+    updateGame(){
+
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
     }
 }
+
+export default Game;
---
+++ W11D1/react_minesweeper.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './frontend/game.jsx';
+
+document.addEventListener("DOMContentLoaded", () => {
+    ReactDOM.render(<Game />, root);
+})
