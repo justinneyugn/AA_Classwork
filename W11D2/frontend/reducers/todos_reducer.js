d2057e9cfe00ee9feb25883ed661f5d818582ae0
amoxley <alex.moxley@gmail.com>
2022-03-08 14:35:04 -0800

    finishes phase 2

--- W11D2/frontend/reducers/todos_reducer.js
+++ W11D2/frontend/reducers/todos_reducer.js
@@ -1,5 +1,39 @@
-const todosReducer = (state = {}, action) => {
+import { RECEIVE_TODOS } from "../actions/todo_actions";
+import { RECEIVE_TODO } from "../actions/todo_actions";
+import { REMOVE_TODO } from "../actions/todo_actions";
+
+const initialState = {
+    1: {
+        id: 1,
+        title: "wash car",
+        body: "with soap",
+        done: false
+    },
+    2: {
+        id: 2,
+        title: "wash dog",
+        body: "with shampoo",
+        done: true
+    }
+};
+
+
+const todosReducer = (state = initialState, action) => {
+    Object.freeze(state);
+    const nextState = Object.assign({}, state);
+
     switch (action.type) {
+        case RECEIVE_TODOS:
+            action.todos.forEach(todo => {
+                nextState[todo.id] = todo; 
+            });
+            return nextState;
+        case RECEIVE_TODO:
+            nextState[action.todo.id] = action.todo; 
+            return nextState;
+        case REMOVE_TODO:
+            delete nextState[action.id];
+            return nextState;
         default:
             return state;
     }
--- W11D2/frontend/todo_redux.jsx
+++ W11D2/frontend/todo_redux.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from "./store/store"
+import { receiveTodo } from "./actions/todo_actions"
+import { receiveTodos } from "./actions/todo_actions"
+import { removeTodo } from "./actions/todo_actions"
 
 document.addEventListener("DOMContentLoaded", () => {
     const content = document.querySelector("#content");
     const h1 = <h1>Todos App</h1>
+    const store = configureStore();
     ReactDOM.render(h1, content);
+
+    window.store = store;
+    window.receiveTodo = receiveTodo;
+    window.receiveTodos = receiveTodos;
+    window.removeTodo = removeTodo;
 })
